b8beca5d5ac41d88c6309b738a5a6342b796f8f5
ronnangelolee <rrlee@student.apc.edu.ph>
2019-12-16 13:51:41 +0800

    presidents, cities, countries, districts, house_numbers, postal_codes, street_names, unit_numbers, provinces tables with sequences and triggers

--- projects3_oracle_view/backup.sql
+++ projects3_oracle_view/backup.sql
@@ -2,6 +2,9 @@
 -- table creations
 --
 
+--
+-- unit numbers
+--
 CREATE TABLE MI182_RRLEE.unit_numbers (
     id NUMBER,
     unit_number VARCHAR2(10),
@@ -12,16 +15,21 @@ CREATE TABLE MI182_RRLEE.unit_numbers (
 CREATE SEQUENCE MI182_RRLEE.unit_numbers_sequence
     MINVALUE 1
     START WITH 1
-    INCREMENT BY 1
     CACHE 20;
 
-ALTER USER MI182_RRLEE quota unlimited on USERS;
+CREATE OR REPLACE TRIGGER MI182_RRLEE.unit_numbers_trigger
+	BEFORE INSERT ON MI182_RRLEE.unit_numbers
+	FOR EACH ROW
 
-INSERT INTO MI182_RRLEE.unit_numbers (unit_number)
-VALUES
-(MI182_RRLEE.unit_numbers_sequence.NEXTVAL, '3A'),
-(MI182_RRLEE.unit_numbers_sequence.NEXTVAL, '3B');
+BEGIN
+	SELECT MI182_RRLEE.unit_numbers_sequence.nextval
+	INTO :NEW.id
+	FROM dual;
+END;
 
+--
+-- house numbers
+--
 CREATE TABLE MI182_RRLEE.house_numbers (
     id NUMBER,
     house_number VARCHAR2(10),
@@ -29,6 +37,24 @@ CREATE TABLE MI182_RRLEE.house_numbers (
     UNIQUE (house_number)
 );
 
+CREATE SEQUENCE MI182_RRLEE.house_numbers_sequence
+    MINVALUE 1
+    START WITH 1
+    CACHE 20;
+
+CREATE OR REPLACE TRIGGER MI182_RRLEE.house_numbers_trigger
+    BEFORE INSERT ON MI182_RRLEE.unit_numbers
+    FOR EACH ROW
+
+BEGIN
+    SELECT MI182_RRLEE.house_numbers_sequence.nextval
+    INTO :NEW.id
+    FROM dual;
+END;
+
+--
+-- street_names
+--
 CREATE TABLE MI182_RRLEE.street_names (
     id NUMBER,
     street_name VARCHAR2(50),
@@ -36,6 +62,24 @@ CREATE TABLE MI182_RRLEE.street_names (
     UNIQUE (street_name)
 );
 
+CREATE SEQUENCE MI182_RRLEE.street_names_sequence
+    MINVALUE 1
+    START WITH 1
+    CACHE 20;
+
+CREATE OR REPLACE TRIGGER MI182_RRLEE.street_names_trigger
+	BEFORE INSERT ON MI182_RRLEE.street_names
+	FOR EACH ROW
+
+BEGIN
+	SELECT MI182_RRLEE.street_names_sequence.nextval
+	INTO :NEW.id
+	FROM dual;
+END;
+
+--
+-- districts
+--
 CREATE TABLE MI182_RRLEE.districts (
     id NUMBER,
     district VARCHAR2(50),
@@ -43,6 +87,24 @@ CREATE TABLE MI182_RRLEE.districts (
     UNIQUE (district)
 );
 
+CREATE SEQUENCE MI182_RRLEE.districts_sequence
+    MINVALUE 1
+    START WITH 1
+    CACHE 20;
+
+CREATE OR REPLACE TRIGGER MI182_RRLEE.districts_trigger
+	BEFORE INSERT ON MI182_RRLEE.districts
+	FOR EACH ROW
+
+BEGIN
+	SELECT MI182_RRLEE.districts_sequence.nextval
+	INTO :NEW.id
+	FROM dual;
+END;
+
+--
+-- cities
+--
 CREATE TABLE MI182_RRLEE.cities (
     id NUMBER,
     city VARCHAR2(50),
@@ -50,6 +112,24 @@ CREATE TABLE MI182_RRLEE.cities (
     UNIQUE (city)
 );
 
+CREATE SEQUENCE MI182_RRLEE.cities_sequence
+    MINVALUE 1
+    START WITH 1
+    CACHE 20;
+
+CREATE OR REPLACE TRIGGER MI182_RRLEE.cities_trigger
+	BEFORE INSERT ON MI182_RRLEE.cities
+	FOR EACH ROW
+
+BEGIN
+	SELECT MI182_RRLEE.cities_sequence.nextval
+	INTO :NEW.id
+	FROM dual;
+END;
+
+--
+-- postal_codes
+--
 CREATE TABLE MI182_RRLEE.postal_codes (
     id NUMBER,
     postal_code VARCHAR2(10),
@@ -57,6 +137,24 @@ CREATE TABLE MI182_RRLEE.postal_codes (
     UNIQUE (postal_code)
 );
 
+CREATE SEQUENCE MI182_RRLEE.postal_codes_sequence
+    MINVALUE 1
+    START WITH 1
+    CACHE 20;
+
+CREATE OR REPLACE TRIGGER MI182_RRLEE.postal_codes_trigger
+	BEFORE INSERT ON MI182_RRLEE.postal_codes
+	FOR EACH ROW
+
+BEGIN
+	SELECT MI182_RRLEE.postal_codes_sequence.nextval
+	INTO :NEW.id
+	FROM dual;
+END;
+
+--
+-- provinces
+--
 CREATE TABLE MI182_RRLEE.provinces (
     id NUMBER,
     province VARCHAR2(10),
@@ -64,6 +162,25 @@ CREATE TABLE MI182_RRLEE.provinces (
     UNIQUE (province)
 );
 
+CREATE SEQUENCE MI182_RRLEE.provinces_sequence
+    MINVALUE 1
+    START WITH 1
+    CACHE 20;
+
+CREATE OR REPLACE TRIGGER MI182_RRLEE.provinces_trigger
+	BEFORE INSERT ON MI182_RRLEE.provinces
+	FOR EACH ROW
+
+BEGIN
+	SELECT MI182_RRLEE.provinces_sequence.nextval
+	INTO :NEW.id
+	FROM dual;
+END;
+
+
+--
+-- countries
+--
 CREATE TABLE MI182_RRLEE.countries (
     id NUMBER,
     country VARCHAR2(10),
@@ -71,6 +188,24 @@ CREATE TABLE MI182_RRLEE.countries (
     UNIQUE (country)
 );
 
+CREATE SEQUENCE MI182_RRLEE.countries_sequence
+    MINVALUE 1
+    START WITH 1
+    CACHE 20;
+
+CREATE OR REPLACE TRIGGER MI182_RRLEE.countries_trigger
+	BEFORE INSERT ON MI182_RRLEE.countries
+	FOR EACH ROW
+
+BEGIN
+	SELECT MI182_RRLEE.countries_sequence.nextval
+	INTO :NEW.id
+	FROM dual;
+END;
+
+--
+-- presidents
+--
 CREATE TABLE MI182_RRLEE.presidents (
     id NUMBER,
     first_name VARCHAR2(50) NOT NULL,
@@ -79,11 +214,11 @@ CREATE TABLE MI182_RRLEE.presidents (
     unit_number NUMBER,
     house_number NUMBER,
     street_name NUMBER,
-    district NUMBER,
-    city NUMBER,
+    district NUMBER NOT NULL,
+    city NUMBER NOT NULL,
     postal_code NUMBER,
-    province NUMBER,
-    country NUMBER,
+    province NUMBER NOT NULL,
+    country NUMBER NOT NULL,
     contact_number VARCHAR2(15),
     email_address VARCHAR2(100),
     PRIMARY KEY (id),
@@ -98,6 +233,27 @@ CREATE TABLE MI182_RRLEE.presidents (
     FOREIGN KEY (country) REFERENCES MI182_RRLEE.countries (id)
 );
 
-CREATE TABLE MI182_RRLEE.clients (
-    id NUMBER,
-);
+CREATE SEQUENCE MI182_RRLEE.presidents_sequence
+    MINVALUE 1
+    START WITH 1
+    CACHE 20;
+
+CREATE OR REPLACE TRIGGER MI182_RRLEE.presidents_trigger
+	BEFORE INSERT ON MI182_RRLEE.presidents
+	FOR EACH ROW
+
+BEGIN
+	SELECT MI182_RRLEE.presidents_sequence.nextval
+	INTO :NEW.id
+	FROM dual;
+END;
+
+--
+-- insert data
+--
+ALTER USER MI182_RRLEE quota unlimited on USERS;
+
+INSERT INTO MI182_RRLEE.unit_numbers (unit_number)
+VALUES
+(MI182_RRLEE.unit_numbers_sequence.NEXTVAL, '3A'),
+(MI182_RRLEE.unit_numbers_sequence.NEXTVAL, '3B');
